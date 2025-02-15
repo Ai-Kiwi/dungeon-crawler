0b60a66c9afc2f290902f2e53765b876e5311450
Ai-Kiwi <contact@aikiwi.dev>
2025-02-15 21:02:01 +1300

    moved draw inventory into different file

--- src/render/render_gui/render_hotbar.rs
+++ src/render/render_gui/render_hotbar.rs
@@ -1,6 +1,6 @@
 use raylib::{color::Color, ffi::Rectangle, prelude::{RaylibDraw, RaylibDrawHandle}};
 
-use crate::{assets::Assets, game_dimension::GameDimension, item, player::{self, Player}, render::{camera::Camera, render_utils::{clickable_item_slot, ClickableItemSlotAction}, MouseInfo, RenderBufferItem}};
+use crate::{assets::Assets, game_dimension::GameDimension, item, player::Player, render::{camera::Camera, render_utils::{clickable_item_slot, ClickableItemSlotAction}, MouseInfo, RenderBufferItem}};
 
 use super::GuiPagesInfo;
 
@@ -34,12 +34,12 @@ pub fn render_hotbar(render_buffer : &mut Vec<RenderBufferItem>, player : &mut P
         }).collect::<Vec<u32>>()
     };
 
-    let slot_size = 64.0;
-    let padding = 5.0;
-    let item_padding = 8.0; // Padding for the item within the slot
+    const SLOT_SIZE: f32 = 64.0;
+    const PADDING: f32 = 5.0;
+    const ITEM_PADDING: f32 = 8.0; // Padding for the item within the slot
     for (index, item) in hotbar.iter().enumerate() {
-        let render_x = padding + (index as f32 * (slot_size + padding)) + item_padding;
-        let render_y = camera.screen_height as f32 - slot_size - padding + item_padding;
+        let render_x = PADDING + (index as f32 * (SLOT_SIZE + PADDING)) + ITEM_PADDING;
+        let render_y = camera.screen_height as f32 - SLOT_SIZE - PADDING + ITEM_PADDING;
         let selected = index == gui_pages_info.hotbar_selected_slot.try_into().unwrap();
         //if amount is 0 delete item from hotbar
         if let Some(_) = item {
@@ -50,39 +50,12 @@ pub fn render_hotbar(render_buffer : &mut Vec<RenderBufferItem>, player : &mut P
         match &gui_pages_info.inventory_selected_item {
             Some(_) => {
                 let safe_item = gui_pages_info.inventory_selected_item.as_ref().unwrap().clone();
-                clickable_item_slot(d, assets, render_x, render_y, &item, &hotbar_item_counts[index], selected, slot_size, padding, ClickableItemSlotAction::ReplaceHotbarItem(index as i32, &safe_item), gui_pages_info, player);
+                clickable_item_slot(d, assets, render_x, render_y, &item, &hotbar_item_counts[index], selected, SLOT_SIZE, PADDING, ClickableItemSlotAction::ReplaceHotbarItem(index as i32, &safe_item), gui_pages_info, player);
             },
             None => {
-                clickable_item_slot(d, assets, render_x, render_y, &item, &hotbar_item_counts[index], selected, slot_size, padding, ClickableItemSlotAction::None, gui_pages_info, player);
+                clickable_item_slot(d, assets, render_x, render_y, &item, &hotbar_item_counts[index], selected, SLOT_SIZE, PADDING, ClickableItemSlotAction::None, gui_pages_info, player);
 
             },
-        }
-    }
-
-    //render inventory assuming opened right now, it will just be hotbar but but repeating for all items
-    //it will also be scrollable and only take up a portion of the screen
-    //render the inventory
-    if gui_pages_info.inventory_open == true {
-            
-        let inventory = player.inventory.clone();
-        let inventory_slot_size = 64.0;
-        let inventory_padding = 5.0;
-        let inventory_slots_per_row = 10;
-        let mut inventory_slot_index = 0;
-        for item in inventory.iter().skip((gui_pages_info.inventory_scroll_offset * inventory_slots_per_row) as usize) {
-            let row = inventory_slot_index / inventory_slots_per_row;
-            let col = inventory_slot_index % inventory_slots_per_row;
-        
-            let render_x = inventory_padding + (col as f32 * (inventory_slot_size + inventory_padding));
-            let render_y = inventory_padding + (row as f32 * (inventory_slot_size + inventory_padding));
-        
-            if render_y > camera.screen_height as f32 - 100.0 {
-                break;
-            }
-            
-            let selected = gui_pages_info.inventory_selected_item.as_ref() == Some(item.0);
-            clickable_item_slot(d, &assets, render_x, render_y, &Some(item.0.clone()), item.1, selected, inventory_slot_size, item_padding, ClickableItemSlotAction::SelectInventoryItem(inventory_slot_index, item.0), gui_pages_info, player);
-            inventory_slot_index += 1;
         }
     }
 
@@ -128,8 +101,8 @@ pub fn render_hotbar(render_buffer : &mut Vec<RenderBufferItem>, player : &mut P
     if offhand_amount == 0 {
         player.off_hand = None;
     }
-    clickable_item_slot(d, &assets, (camera.screen_width - 69) as f32, (camera.screen_height - 69 - 64 - 5) as f32, &main_hand, &main_hand_amount, false, 64.0, item_padding, ClickableItemSlotAction::None, gui_pages_info, player);
-    clickable_item_slot(d, &assets, (camera.screen_width - 69) as f32, (camera.screen_height - 69) as f32, &off_hand, &offhand_amount, false, 64.0, item_padding, ClickableItemSlotAction::None, gui_pages_info, player);
+    clickable_item_slot(d, &assets, (camera.screen_width - 69) as f32, (camera.screen_height - 69 - 64 - 5) as f32, &main_hand, &main_hand_amount, false, 64.0, ITEM_PADDING, ClickableItemSlotAction::None, gui_pages_info, player);
+    clickable_item_slot(d, &assets, (camera.screen_width - 69) as f32, (camera.screen_height - 69) as f32, &off_hand, &offhand_amount, false, 64.0, ITEM_PADDING, ClickableItemSlotAction::None, gui_pages_info, player);
     //make sure they still have the item in their inventory, otherwise remove it
     
     //draw bars
