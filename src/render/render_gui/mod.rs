7d80006392e3e439be66c4d41da9d3ec195ed6c2
Ai-Kiwi <contact@aikiwi.dev>
2025-02-15 21:08:34 +1300

    cleaned up code a bit for pre release

--- src/render/render_gui/mod.rs
+++ src/render/render_gui/mod.rs
@@ -1,5 +1,7 @@
 pub mod render_hotbar;
 pub mod render_inventory;
+use raylib::color::Color;
+use raylib::prelude::RaylibDraw;
 use raylib::{ffi::MouseButton, prelude::RaylibDrawHandle};
 
 use crate::{assets::Assets, game_dimension::GameDimension, item::Item, player::Player, render::render_gui::render_hotbar::render_hotbar};
@@ -35,4 +37,16 @@ pub fn render_ui(render_buffer : &mut Vec<RenderBufferItem>, player : &mut Playe
     render_hotbar(render_buffer, player, game_dimension, camera, d, assets, &mouse_info, gui_pages_info);
 
     render_inventory(gui_pages_info, player, camera, assets, d);
+
+    if gui_pages_info.inventory_open == false{
+        d.draw_rectangle(0, 55, 400, 120, Color::new(50,50,50,100));
+        d.draw_text("Kiwi Crawler pre release", 5, 60, 25, Color::WHITE);
+        d.draw_text("W,A,S,D : Used for moving", 5, 80, 15, Color::WHITE);
+        d.draw_text("I : open inventory", 5, 95, 15, Color::WHITE);
+        d.draw_text("P : pickup items", 5, 110, 15, Color::WHITE);
+        d.draw_text("TAB : swap current item between off hand and main", 5, 125, 15, Color::WHITE);
+        d.draw_text("alt + scroll : zoom in/out", 5, 140, 15, Color::WHITE);
+        d.draw_text("Scroll : change selected slot and scroll in inventory", 5, 155, 15, Color::WHITE);
+    }
+
 }
